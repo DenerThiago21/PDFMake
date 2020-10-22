68f576e364919ff56d0f6bf0be5b7b69c605821c
DenerThiago21 <denerthi.ibi@gmail.com/>
2020-10-22 17:30:00 -0300

    table Adjustment and more

--- index.js
+++ index.js
@@ -1,67 +1,60 @@
 const fonts = {
-    Roboto: {
-      normal: 'fonts/Roboto/Roboto-Regular.ttf',
-      bold: 'fonts/Roboto/Roboto-Medium.ttf',
-      italics: 'fonts/Roboto/Roboto-Italic.ttf',
-      bolditalics: 'fonts/Roboto/Roboto-MediumItalic.ttf'
-    }
-  };
+  Times: {
+    normal: 'Times-Roman',
+    bold: 'Times-Bold',
+    italics: 'Times-Italic',
+    bolditalics: 'Times-BoldItalic'
+  },
+  // Roboto: {
+  //   normal: 'fonts/Roboto/Roboto-Regular.ttf',
+  //   bold: 'fonts/Roboto/Roboto-Medium.ttf',
+  //   italics: 'fonts/Roboto/Roboto-Italic.ttf',
+  //   bolditalics: 'fonts/Roboto/Roboto-MediumItalic.ttf'
+  // }
+};
 
- const fs = require('fs');
+const fs = require('fs');
 const PdfPrinter = require('pdfmake');
 const printer = new PdfPrinter(fonts);
 const obj = JSON.parse(fs.readFileSync('./db/data.json', 'utf8'));
 
 const filters = 'Cidade: Ibirama, Curso: Enfermagem';
 
-var row = [['Modalidade', 'Etapas/Curso', 'Tipo de Mediação', 'Parecer', 'Portaria' ]];
+//var row = [['Modalidade', 'Etapas/Curso', 'Tipo de Mediação', 'Parecer', 'Portaria' ]];
+const headers = [{text: 'Modalidade', bold: true},
+{text: 'Etapas/Curso', bold: true},
+{text: 'Tipo de Mediação', bold: true},
+{text: 'Parecer', bold: true},
+{text: 'Portaria', bold: true}];
+var row = [];
+row.push(headers);
 
 function percorrer(obj) {
   for (let i in obj) {
     let curses = [];
-    let institution = [{text: [{text: 'Instituição: ', bold: true}, obj[i].instituicaoNome], colSpan: 5}];
+    let institution = [{text: [{text: 'Instituição: ', bold: true}, obj[i].instituicaoNome], colSpan: 5, fillColor: '#CCCCCC'}];
     row.push(institution);
     const acts = obj[i].atos;
     for(let j in acts) {
-      let listCurse = acts[i].Curso;
+      let listCurse = acts[j].Curso;
       for(let k in listCurse) {
         curses.push(listCurse[k]);
       }
       const temp = [acts[j].modalidade, curses, acts[j].mediacao, acts[j].parecer, acts[j].portaria];
       row.push(temp);
+      curses = [];
     }
+    row.push([ {text: '', colSpan: 5} ])
   }
+  //DESENHAR LINHA MAIS ESCURA PARA  DIVIDIR
 }
-    
-
-
-
-  //   if (obj.hasOwnProperty(propriedade)) {
-  //     if (typeof obj[propriedade] == "object") {
-  //       percorrer(obj[propriedade]);
-  //     } else {
-  //       if(propriedade == 'instituicaoNome') {
-  //         resultado.push([])
-  //       } else {
-  //         if(propriedade == 'Curso'){
-  //           console.log(propriedade);
-  //           console.log(typeof(propriedade));
-  //         }
-          
-  //       }
-  //       resultado.push(obj[propriedade]);
-  //     }
-  //   }
-  // }
-//}
 
 percorrer(obj);
-console.log(JSON.stringify(row));
 
 const dd = {
     pageSize: 'A4',
     pageOrientation: 'landscape',
-    pageMargins: [65, 70, 65, 60],
+    pageMargins: [65, 100, 80, 65],
     header: 'teste',
     header: {
         columns: [
@@ -71,44 +64,45 @@ const dd = {
                 height: 41.25,
             },
             {
-                text: [{text: 'Relatório Sintético', style: 'titleStyle'},
-                       {text: `\n${filters}`, fontSize: 10}],
-                margin: [160, 0.8]
+                text: [{text: 'Relatório de Atos - Sintético', style: 'titleStyle'},
+                       {text: `\n${filters}`, fontSize: 10, bold: false, style: 'titleStyle'}],
+                margin: [160, 1]
                 
             }
           ],
-          margin: [65, 35],
-          // canvas: [{ type: 'line', x1: 10, y1: 10, x2: 595-10, y2: 10, lineWidth: 1 }],
+          margin: [65, 25]
     }, 
     content: [
         {
-          layout: 'lightHorizontalLines', // optional
+          layout: 'lightHorizontalLines',
           table: {
             // headers are automatically repeated if the table spans over multiple pages
             // you can declare how many rows should be treated as headers
             headerRows: 1,
-            widths: [ '*', '*', '*', '*', '*' ],
+            widths: [ 100, '*', 100, '*', '*' ],
     
             body: row
-              // [institution],
-              // first[0],
-              // first[1],
-              // [ {text: '', colSpan: 5} ],
-              // [{text: [{text: 'Instituição: ', bold: true}, 'ABC'], colSpan: 5}],
-              // [ 'Value 1', ['Value 2', 'value2.1', 'value2.3'], 'Value 3', 'Value 4', 'value 5' ],
-              // [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4', 'Val 5' ],
-            , 
-          }
+          },
+          style: 'defaultStyle'
         }
       ],
       styles: {
         titleStyle: {
             fontSize: 18,
+            font: 'Times',
             bold: true
+        },
+        defaultStyle: {
+          fontSize: 12,
+          font: 'Times'
+
         },  
         tableStyle: {
-            margin: [80, 100, 65, 60]
-        }       
+            alignment: 'center'
+        },   
+        tableHeaderStyle: {
+          bold: true,
+        }
       }
         
 }
